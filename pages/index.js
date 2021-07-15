79c4ee4947fa418107a5f6a6a9ef7567b313b9ce
Hitesh Khatri <hitesh.khatri@bmigroup.com>
2021-07-15 21:28:16 +0100

    lesson finished

--- pages/index.js
+++ pages/index.js
@@ -25,7 +25,6 @@ export default function Home ({ postList }) {
 
 export async function getServerSideProps () {
   const postList = await githubCms.getPostList()
-
   return {
     props: {
       postList
